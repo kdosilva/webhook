13840ff8ac90a753732dd3c99a4481de1b0934d2
kdosilva <142738464+kdosilva@users.noreply.github.com>
2025-07-26 16:19:06 -0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,9 +1,4 @@
 <?php
-// Z-API configs
-$zapi_instance = '3E401062FA83E0F253FEBE7C53096139'; // Ex: 3FXXXXX9F
-$zapi_token = '021056C63BB7C732FB534BCD';           // Ex: abc123
-
-<?php
 // Recebe e loga todo o conteúdo
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
@@ -18,4 +13,3 @@ if (!$data) {
 } else {
     echo "✅ Dados recebidos";
 }
-
